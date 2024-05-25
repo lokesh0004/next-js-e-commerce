9865ff26ef5c2e909605c202d49038608a2823e1
Lokesh Sengar <143507709+lokesh0004@users.noreply.github.com>
2024-05-26 02:57:09 +0530

    Add files via upload

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,18 +1,19 @@
+"use client"
 import React from 'react'
 
 const Header = () => {
   return (
   <>
-  <div className='h-32 bg-black flex items-center justify-between text-white text-3xl font-bold'>
-    <div><h2>WEshop</h2>
-    <div><img src='.C:\Users\rajpu\OneDrive\Desktop\project-1\app\components\download.jfif'></img></div>
+  <div className='h-56 bg-black flex items-center justify-between font-sans text-white text-3xl font-bold'>
+    <div><h2>Weshop</h2>
+    <div><img src=""></img></div>
       </div>
-    <div className='text-white font-bold flex items-center gap-3 text-3xl'>
-      <h4>About</h4>
-      <h4>Carrier</h4>
-      <h4>Blog</h4>
-      <h4>Account</h4>
-      <button type="button" className='bg-slate-100 text-black text-2xl rounded-xl'>Login/signup</button>
+    <div className='text-white font-bold font-sans flex items-center gap-3 text-3xl'>
+      <h3>About</h3>
+      <h3>Carrier</h3>
+      <h3>Blog</h3>
+      <h3>Account</h3>
+      <button type="button" className='bg-slate-50 hover:bg-slate-300 ml-2 p-2 w-20.5 mr-3 font-sans text-black text-2xl rounded-3xl'>Login/signup</button>
     </div>
   </div>
   </>
